08943da408de26c5ab3608835c2ab5a51441cbce
Seonghyeon Kim <kshshkim@users.noreply.github.com>
2023-10-25 12:07:03 +0900

    Init: Initial commit

---
+++ src/test/java/beforespring/socialfeed/SocialFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package beforespring.socialfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialFeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
