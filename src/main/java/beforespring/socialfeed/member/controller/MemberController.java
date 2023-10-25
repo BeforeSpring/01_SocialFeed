8ebcedef94a1c04346868e1b5e02c6855752acb4
Danbi <ddanhopark@gmail.com>
2023-10-25 23:31:16 +0900

    Refactor: rename requestId to memberId
    createMember에서 join의 반환값 변수명을 명확하게 수정함.
    Github issue #3

--- src/main/java/beforespring/socialfeed/member/controller/MemberController.java
+++ src/main/java/beforespring/socialfeed/member/controller/MemberController.java
@@ -3,10 +3,8 @@ package beforespring.socialfeed.member.controller;
 import beforespring.socialfeed.member.controller.dto.CreateMemberDto;
 import beforespring.socialfeed.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -18,14 +16,14 @@ public class MemberController {
     private final MemberService memberService;
 
     /**
-     *멤버 생성. 가입 요청과 가입 승인 서비스를 호출합니다.
+     * 멤버 생성. 가입 요청과 가입 승인 서비스를 호출합니다.
      *
      * @param request 멤버 생성을 위한 dto
      * @return 생성된 멤버의 아이디를 반환합니다.
      */
     @PostMapping("/api/member/new")
     public CreateMemberDto.Response createMember(@RequestBody @Valid CreateMemberDto.Request request) {
-        Long requestId = memberService.join(request);
-        return new CreateMemberDto.Response(requestId);
+        Long memberId = memberService.join(request);
+        return new CreateMemberDto.Response(memberId);
     }
 }
