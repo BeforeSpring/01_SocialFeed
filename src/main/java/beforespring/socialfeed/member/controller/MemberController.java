eaec1e82691981c75bd35710088c6157a6555bc8
Danbi <ddanhopark@gmail.com>
2023-10-25 19:44:06 +0900

    Init: Add member controller
    createMember 컨트롤러를 생성함.
    파라미터로 패스워드와 이메일 유효성 검증이 된 dto를 받음
    Github issue #3

--- src/main/java/beforespring/socialfeed/member/controller/MemberController.java
+++ src/main/java/beforespring/socialfeed/member/controller/MemberController.java
@@ -1,2 +1,24 @@
-package beforespring.socialfeed.member.controller;public class MemberController {
+package beforespring.socialfeed.member.controller;
+
+import beforespring.socialfeed.member.controller.dto.CreateMemberDto;
+import beforespring.socialfeed.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+    @PostMapping("/api/member/new")
+    public CreateMemberDto.Response createMember(@RequestBody @Valid CreateMemberDto.Request request) {
+        Long requestId = memberService.join(request);
+        return new CreateMemberDto.Response(requestId);
+    }
 }
