be1df9d76f8f756e281a45e0e1d847a42873ea92
jaekkang <rkdwornjs123@gmail.com>
2023-10-26 14:02:16 +0900

    Init: Create member repository
    멤버 데이터를 저장하기 위한 저장소를 만들었습니다.
    기본적인 영속성 추가를 위한
    save()와 단건 데이터 검색과 전체 조회 메서드를 추가했습니다.

---
+++ src/main/java/beforespring/socialfeed/member/repository/MemberRepository.java
@@ -0,0 +1,28 @@
+package beforespring.socialfeed.member.repository;
+
+import beforespring.socialfeed.member.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+    private final EntityManager em;
+
+    public void save(Member member) {   
+        em.persist(member);
+    }
+
+    public Member findByName(String name) {
+        return em.find(Member.class, name);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+
+}
