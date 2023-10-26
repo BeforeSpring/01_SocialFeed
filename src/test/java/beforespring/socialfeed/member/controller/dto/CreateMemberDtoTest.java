6ee3ce681f8de0dd39c164dbfd3b4001cccd1087
Danbi <ddanhopark@gmail.com>
2023-10-26 11:38:39 +0900

    Test: Change expectable exception message
    dto에서 발생하는 에러가 변경됨에따라 에러 메세지를 수정함
    Github issue #3

--- src/test/java/beforespring/socialfeed/member/controller/dto/CreateMemberDtoTest.java
+++ src/test/java/beforespring/socialfeed/member/controller/dto/CreateMemberDtoTest.java
@@ -1,11 +1,11 @@
 package beforespring.socialfeed.member.controller.dto;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import beforespring.socialfeed.member.controller.dto.CreateMemberDto.Request;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CreateMemberDtoTest {
     @Test
@@ -22,18 +22,19 @@ class CreateMemberDtoTest {
                     givenUsername,
                     givenEmail,
                     givenPassword
-                    )
+                )
         )
             .describedAs("예외가 발생하지 않고 생성에 성공할것.")
             .doesNotThrowAnyException();
     }
+
     @Test
     @DisplayName("3회 이상 연속되는 문자는 사용이 불가능합니다.")
     void password_validation_passwd_not_valid_consecutive_character() {
         // given
         String givenUsername = "givenUsername";
         String givenEmail = "givenEmail@gmail.com";
-        String givenPassword = "paaaswd1!"; // a가 3번 반복되는 잘못된 패스워드
+        String givenPassword = "paaaaswd1!!"; // a가 3번 반복되는 잘못된 패스워드
 
         // when then
         assertThatThrownBy(
@@ -45,7 +46,7 @@ class CreateMemberDtoTest {
                 )
         )
             .describedAs("3회 이상 반복되는 문자에 대해서 예외가 발생해야함.")
-            .hasMessageContaining("3회 이상 연속되는 문자는 사용이 불가합니다.");
+            .hasMessageContaining("동일한 문자를 3회 이상 연속으로 사용할 수 없습니다.");
     }
 
     @Test
@@ -55,7 +56,6 @@ class CreateMemberDtoTest {
         String givenUsername = "givenUsername";
         String givenEmail = "givenEmail@gmail.com";
         String givenPassword = "passwordNotOkay"; // 숫자와 특수문자가 없는 잘못된 패스워드
-
         // when then
         assertThatThrownBy(
             () ->
@@ -66,6 +66,6 @@ class CreateMemberDtoTest {
                 )
         )
             .describedAs("숫자, 문자, 특수문자 중 2가지 이상을 포함해야함.")
-            .hasMessageContaining("숫자, 문자, 특수문자 중 2가지 이상을 포함해야");
+            .hasMessageContaining("숫자, 문자, 특수문자 중 2가지 이상을 포함해야 합니다.");
     }
 }
