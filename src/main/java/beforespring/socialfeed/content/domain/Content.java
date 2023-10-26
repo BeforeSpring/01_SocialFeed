d54ac856f0475f740224b7eab96a1bd17c517da9
Seonghyeon Kim <kshshkim@users.noreply.github.com>
2023-10-26 12:25:54 +0900

    Feat: Content, HashtagContent 인덱싱, JPA 관련 어노테이션 수정, 필드 추가 (#31)
    - 누락된 Source ID 추가
    - RDB에 적합한 컬럼명 지정
    - 쿼리에 사용될 수 있는 인덱스 추가
    
    Ref: #30

--- src/main/java/beforespring/socialfeed/content/domain/Content.java
+++ src/main/java/beforespring/socialfeed/content/domain/Content.java
@@ -1,18 +1,28 @@
 package beforespring.socialfeed.content.domain;
 
 
-import lombok.*;
-
-import javax.persistence.*;
 import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(
     name = "content",
     indexes = {
         @Index(
-            name = "idx__content_tuple",
-            columnList = "content_id, content_source_type",
+            name = "idx__content__content_source_id__content_source_type",
+            columnList = "content_source_id, content_source_type",
             unique = true
         )
     }
@@ -20,21 +30,39 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Content {
+
     @Id
-    @Column(name = "memberId")
+    @Column(name = "content_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * <p>외부 시스템에서 관리되는 ID로, unique함이 보장되지 않음. unique를 보장하려면 source와 함께 묶어야함.</p>
+     */
+    @Column(name = "content_source_id")
+    private String contentSourceId;
+
     @Enumerated(EnumType.STRING)
+    @Column(name = "content_source_type")
     private ContentSourceType contentSourceType;
 
     private String title;
+
     private String content;
+
+    @Column(name = "view_count")
     private Long viewCount;
+
+    @Column(name = "like_count")
     private Long likeCount;
+
+    @Column(name = "share_count")
     private Long shareCount;
 
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
     public void incrementViewCount() {
@@ -47,5 +75,30 @@ public class Content {
 
     public void incrementShareCount() {
         this.shareCount++;
+    }
+
+    @Builder
+    protected Content(
+        Long id,
+        String contentSourceId,
+        ContentSourceType contentSourceType,
+        String title,
+        String content,
+        Long viewCount,
+        Long likeCount,
+        Long shareCount,
+        LocalDateTime updatedAt,
+        LocalDateTime createdAt
+    ) {
+        this.id = id;
+        this.contentSourceId = contentSourceId;
+        this.contentSourceType = contentSourceType;
+        this.title = title;
+        this.content = content;
+        this.viewCount = viewCount;
+        this.likeCount = likeCount;
+        this.shareCount = shareCount;
+        this.updatedAt = updatedAt;
+        this.createdAt = createdAt;
     }
 }
