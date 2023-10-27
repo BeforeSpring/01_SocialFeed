78f7e9538db063db95cf67cdf335ad4b841910c4
Seonghyeon Kim <kshshkim@users.noreply.github.com>
2023-10-27 15:17:51 +0900

    Revert "Add member repository"

--- src/main/java/beforespring/socialfeed/member/domain/Member.java
+++ src/main/java/beforespring/socialfeed/member/domain/Member.java
@@ -2,8 +2,6 @@ package beforespring.socialfeed.member.domain;
 
 import beforespring.socialfeed.member.exception.PasswordMismatchException;
 import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -20,7 +18,6 @@ import javax.persistence.*;
     }
 )
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter
 public class Member {
 
     @Id
@@ -32,38 +29,22 @@ public class Member {
     @Column(nullable = false)
     private String password;
 
-    @Builder
-    protected Member(
-        Long id,
-        String username,
-        String raw,
-        PasswordHasher hasher
-    ) {
-        this.id = id;
+    protected Member(String username, String password) {
         this.username = username;
-        this.password = hasher.hash(raw);
+        this.password = password;
     }
 
-    /**
-     * 입력받은 rawPassword를 Hasher를 통해 변환했을 때,
-     * 저장된 password와 같은 지 확인하는 메서드
-     *
-     * @param rawPassword
-     * @param hasher
-     */
-    public void verifyPassword(String rawPassword, PasswordHasher hasher) {
+    static public Member create(String username, String password) {
+        return new Member(username, password);
+    }
+
+    public void validatePassword(String rawPassword, PasswordValidator validator, PasswordHasher hasher) {
+        validator.validate(this, rawPassword, hasher);
         if (!hasher.matches(rawPassword, password)) {
             throw new PasswordMismatchException();
         }
     }
 
-    /**
-     * 입력받은 비밀번호의 유효성을 검사한 후에
-     * 올바르면 업데이트하는 메서드
-     *
-     * @param rawPassword
-     * @param hasher
-     */
     public void updatePassword(String rawPassword, PasswordValidator validator, PasswordHasher hasher) {
         validator.validate(this, rawPassword, hasher);
         this.password = hasher.hash(rawPassword);
