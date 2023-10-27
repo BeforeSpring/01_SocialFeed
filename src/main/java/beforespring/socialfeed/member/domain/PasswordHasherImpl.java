70a549751da5111ebf30639db6bd91b435b21099
Danbi <ddanhopark@gmail.com>
2023-10-27 13:47:06 +0900

    Init: Add TokenMismatchException
    인증코드가 일치하지 않을 시 발생하는 에러를 추가함
    Github issue #33

--- src/main/java/beforespring/socialfeed/member/domain/PasswordHasherImpl.java
+++ src/main/java/beforespring/socialfeed/member/domain/PasswordHasherImpl.java
@@ -1,13 +1,2 @@
-package beforespring.socialfeed.member.domain;
-
-public class PasswordHasherImpl implements PasswordHasher {
-    @Override
-    public String hash(String toHash) {
-        return toHash;
-    }
-
-    @Override
-    public boolean matches(String raw, String hashed) {
-        return hash(raw).equals(hashed);
-    }
+package beforespring.socialfeed.member.domain;public class PasswordHasherImpl {
 }
