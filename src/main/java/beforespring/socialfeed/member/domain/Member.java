5072d425c6bd5485065e21f65a4c7ef658f34704
jaekkang <rkdwornjs123@gmail.com>
2023-10-27 14:58:00 +0900

    Style: Change method name
    - validatePassword를 의미적으로 좀 더 맞는
    verifyPassword로 변경되었습니다.
    
    - 메서드에 대한 설명도 추가되었습니다.

--- src/main/java/beforespring/socialfeed/member/domain/Member.java
+++ src/main/java/beforespring/socialfeed/member/domain/Member.java
@@ -44,13 +44,28 @@ public class Member {
         this.password = hasher.hash(raw);
     }
 
-    public void validatePassword(String rawPassword, PasswordValidator validator, PasswordHasher hasher) {
+    /**
+     * 입력받은 rawPassword를 Hasher를 통해 변환했을 때,
+     * 저장된 password와 같은 지 확인하는 메서드
+     *
+     * @param rawPassword
+     * @param hasher
+     */
+    public void verifyPassword(String rawPassword,PasswordHasher hasher) {
         if (!hasher.matches(rawPassword, password)) {
             throw new PasswordMismatchException();
         }
     }
 
-    public void updatePassword(String rawPassword, PasswordValidator validator, PasswordHasher hasher) {
+    /**
+     * 입력받은 비밀번호의 유효성을 검사한 후에
+     * 올바르면 업데이트하는 메서드
+     *
+     * @param rawPassword
+     * @param hasher
+     */
+    public void updatePassword(String rawPassword, PasswordValidator validator , PasswordHasher hasher) {
+        validator.validate(this, rawPassword, hasher);
         this.password = hasher.hash(rawPassword);
     }
 }
