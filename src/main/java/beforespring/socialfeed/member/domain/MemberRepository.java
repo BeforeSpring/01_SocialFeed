02aae6da8c92f07cc33794e57e3cc67bb5d239da
jaekkang <rkdwornjs123@gmail.com>
2023-10-27 13:18:45 +0900

    Style: Change method name
    잘못 작성한 메서드의 이름을 변경하였습니다.
    findByName -> findByusername

--- src/main/java/beforespring/socialfeed/member/domain/MemberRepository.java
+++ src/main/java/beforespring/socialfeed/member/domain/MemberRepository.java
@@ -1,11 +1,9 @@
 package beforespring.socialfeed.member.domain;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByName(String name);
+    Member findByUsername(String name);
 }
