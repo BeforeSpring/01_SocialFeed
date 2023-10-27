b04a2e7414348ed5220add6b09cd6a4afc357282
jaekkang <rkdwornjs123@gmail.com>
2023-10-27 14:40:19 +0900

    Test: remove default method
    빌더를 통한 객체 생성 테스트를 제외하고 jpaRepository에서 자동 생성되는 메서드의 테스트는 삭제했습니다.

--- src/test/java/beforespring/socialfeed/member/domain/MemberRepositoryTest.java
+++ src/test/java/beforespring/socialfeed/member/domain/MemberRepositoryTest.java
@@ -33,38 +33,4 @@ class MemberRepositoryTest {
         assertEquals("User1", member.getUsername());
         assertEquals("1234", member.getPassword());
     }
-
-    @Test
-    @DisplayName("멤버 객체 저장")
-    public void save_member() {
-        // given
-        PasswordHasher hasher = new PasswordHasherImpl();
-        Member member = Member.builder()
-                            .username("User1")
-                            .raw("1234")
-                            .id(1L)
-                            .hasher(hasher)
-                            .build();
-
-        memberRepository.save(member);
-
-        // when & then
-        Member findMember = memberRepository.findById(1L).orElseGet(Member::new);
-        System.out.println(findMember.toString());
-
-        assertEquals(member.getUsername(), findMember.getUsername(), "빌드된 데이터의 유저 정보가 User1으로 잘 불러와져야됩니다.");
-    }
-
-    @Test
-    @DisplayName("존재하지 않는 멤버 조회")
-    public void find_non_existent_member() {
-        // given
-        long nonExistentId = 999L;
-
-        // when
-        Optional<Member> findMember = memberRepository.findById(nonExistentId);
-
-        // then
-        assertTrue(findMember.isEmpty(), "존재하지 않는 멤버 ID로 조회하면 결과가 없어야 합니다.");
-    }
 }
