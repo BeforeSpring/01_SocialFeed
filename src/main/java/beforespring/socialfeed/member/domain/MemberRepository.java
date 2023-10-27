7e691acace7216ce6e886cd868ccf99bc0bb57f9
jaekkang <rkdwornjs123@gmail.com>
2023-10-27 14:39:22 +0900

    Feat: Add optional for empty data
    Repository에서 서치시 데이터가 없을 경우를 위해 Optional로 감쌋습니다.

--- src/main/java/beforespring/socialfeed/member/domain/MemberRepository.java
+++ src/main/java/beforespring/socialfeed/member/domain/MemberRepository.java
@@ -3,7 +3,9 @@ package beforespring.socialfeed.member.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByUsername(String name);
+    Optional<Member> findByUsername(String name);
 }
