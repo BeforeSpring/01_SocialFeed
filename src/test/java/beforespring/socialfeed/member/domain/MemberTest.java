746e308b6812bcf759cd27d0927c453e9dc6fbbd
jaekkang <rkdwornjs123@gmail.com>
2023-10-27 21:14:02 +0900

    Test: Add test for BcryptHasher
    다른 구현체로도 동일하게 동작하는 지에 대한 테스트를 한개 추가했습니다.

--- src/test/java/beforespring/socialfeed/member/domain/MemberTest.java
+++ src/test/java/beforespring/socialfeed/member/domain/MemberTest.java
@@ -1,22 +1,25 @@
 package beforespring.socialfeed.member.domain;
 
 
+import beforespring.socialfeed.member.infra.BcryptHasher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @DataJpaTest
 class MemberTest {
 
-    private PasswordHasher hasher;
+    private PasswordHasher defaultHasher;
+    private PasswordHasher bcryptHasher;
 
     @BeforeEach
     public void setup() {
-        hasher = new PasswordHasher() {
+        defaultHasher = new PasswordHasher() {
             @Override
             public String hash(String toHash) {
                 return toHash;
@@ -27,6 +30,8 @@ class MemberTest {
                 return hash(raw).equals(hashed);
             }
         };
+
+        bcryptHasher = new BcryptHasher();
     }
 
     @Test
@@ -38,12 +43,27 @@ class MemberTest {
                             .username("User1")
                             .raw("1234")
                             .id(1L)
-                            .hasher(hasher)
+                            .hasher(defaultHasher)
                             .build();
 
         // when then
         assertEquals(1L, member.getId());
         assertEquals("User1", member.getUsername());
         assertEquals("1234", member.getPassword());
+    }
+
+    @Test
+    @DisplayName("다른 hasher를 이용해 빌드를 해도 정상적으로 생성되어야합니다.")
+    public void create_member_use_another_hasher() {
+        Member member = Member.builder()
+                            .username("User1")
+                            .raw("1234")
+                            .id(1L)
+                            .hasher(bcryptHasher)
+                            .build();
+
+        assertEquals(1L, member.getId());
+        assertEquals("User1", member.getUsername());
+        assertTrue(bcryptHasher.isMatch("1234", member.getPassword()));
     }
 }
