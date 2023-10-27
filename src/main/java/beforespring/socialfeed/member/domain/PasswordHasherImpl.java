ca4d8ffc499b2657cfb83677a8a1aef6249545d3
Danbi <ddanhopark@gmail.com>
2023-10-27 13:54:47 +0900

    Init: Add passwordhasherImpl
    paasswordHasher 구현체 추가
    Github issue #33

--- src/main/java/beforespring/socialfeed/member/domain/PasswordHasherImpl.java
+++ src/main/java/beforespring/socialfeed/member/domain/PasswordHasherImpl.java
@@ -1,2 +1,13 @@
-package beforespring.socialfeed.member.domain;public class PasswordHasherImpl {
+package beforespring.socialfeed.member.domain;
+
+public class PasswordHasherImpl implements PasswordHasher {
+    @Override
+    public String hash(String toHash) {
+        return toHash;
+    }
+
+    @Override
+    public boolean matches(String raw, String hashed) {
+        return hash(raw).equals(hashed);
+    }
 }
