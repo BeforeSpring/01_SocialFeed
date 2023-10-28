d91331c072c8119462d77fdc21a4898a3c4d5392
Danbi <ddanhopark@gmail.com>
2023-10-28 19:20:13 +0900

    Refactor: findByMember returns Optional
    findByMember가 Optional<Confirm>을 반환하도록 변경함

--- src/main/java/beforespring/socialfeed/member/domain/ConfirmRepository.java
+++ src/main/java/beforespring/socialfeed/member/domain/ConfirmRepository.java
@@ -3,7 +3,9 @@ package beforespring.socialfeed.member.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ConfirmRepository extends JpaRepository<Confirm, Long> {
-    Confirm findByMember(Member member);
+    Optional<Confirm> findByMember(Member member);
 }
