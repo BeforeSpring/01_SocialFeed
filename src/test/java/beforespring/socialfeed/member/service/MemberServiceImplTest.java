2bef2f775f2b0f3fdd823383d6b4340ec7760292
Danbi <ddanhopark@gmail.com>
2023-10-28 21:45:58 +0900

    Test: custom exception, change wrong parameter
    custom exception 추가, confrimTokenRequest에 잘못된 인자를 수정함

--- src/test/java/beforespring/socialfeed/member/service/MemberServiceImplTest.java
+++ src/test/java/beforespring/socialfeed/member/service/MemberServiceImplTest.java
@@ -4,6 +4,8 @@ import beforespring.socialfeed.member.domain.*;
 import beforespring.socialfeed.member.exception.PasswordMismatchException;
 import beforespring.socialfeed.member.exception.TokenMismatchException;
 import beforespring.socialfeed.member.infra.TokenSenderImpl;
+import beforespring.socialfeed.member.service.exception.ConfirmNotFoundException;
+import beforespring.socialfeed.member.service.exception.MemberNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,42 +32,6 @@ class MemberServiceImplTest {
     private CreateMemberRequest createMemberRequest;
     private ConfirmTokenRequest confirmTokenRequest;
 
-    @Test
-    @DisplayName("회원가입한 유저의 이름이 저장된 이름과 동일해야 함.")
-    void member_join_test() {
-        //given
-        String givenUsername = "givenUsername";
-        String givenEmail = "givenEmail@gmail.com";
-        String givenPassword = "passwdThatShou!dBe0kay";
-        CreateMemberRequest request = new CreateMemberRequest(givenUsername, givenEmail, givenPassword);
-
-        //when
-        Long memberId = memberService.join(request);
-        Member findMember = memberRepository.findById(memberId).orElseThrow(IllegalAccessError::new);
-
-        //then
-        assertThat(findMember.getUsername()).isEqualTo(givenUsername);
-    }
-
-    @Test
-    @DisplayName("confirm에 6자리 승인코드가 저장되어야 함.")
-    void create_confirm_test() {
-        //given
-        String givenUsername = "givenUsername";
-        String givenEmail = "givenEmail@gmail.com";
-        String givenPassword = "passwdThatShou!dBe0kay";
-        CreateMemberRequest request = new CreateMemberRequest(givenUsername, givenEmail, givenPassword);
-
-        //when
-        Long memberId = memberService.join(request);
-        Member findMember = memberRepository.findById(memberId).orElseThrow(IllegalAccessError::new);
-        Confirm findConfirm = confirmRepository.findByMember(findMember);
-
-        //then
-        assertThat(findConfirm.getToken().length()).isEqualTo(6);
-
-    }
-
     @BeforeEach
     public void setup() {
         String givenUsername = "givenUsername";
@@ -73,8 +39,31 @@ class MemberServiceImplTest {
         String givenPassword = "passwdThatShou!dBe0kay";
 
         createMemberRequest = new CreateMemberRequest(givenUsername, givenEmail, givenPassword);
+    }
 
+    @Test
+    @DisplayName("회원가입한 유저의 이름이 저장된 이름과 동일해야 함.")
+    void member_join_test() {
 
+        //when
+        Long memberId = memberService.join(createMemberRequest);
+        Member findMember = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
+
+        //then
+        assertThat(findMember.getUsername()).isEqualTo(createMemberRequest.getUsername());
+    }
+
+    @Test
+    @DisplayName("confirm에 6자리 승인코드가 저장되어야 함.")
+    void create_confirm_test() {
+
+        //when
+        Long memberId = memberService.join(createMemberRequest);
+        Member findMember = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
+        Confirm findConfirm = confirmRepository.findByMember(findMember).orElseThrow(ConfirmNotFoundException::new);
+
+        //then
+        assertThat(findConfirm.getToken().length()).isEqualTo(6);
     }
 
     @Test
@@ -82,12 +71,12 @@ class MemberServiceImplTest {
     void join_confirm_test() {
         //given
         Long memberId = memberService.join(createMemberRequest);
-        Member findMember = memberRepository.findById(memberId).orElseThrow(IllegalAccessError::new);
-        Confirm findConfirm = confirmRepository.findByMember(findMember);
+        Member findMember = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
+        Confirm findConfirm = confirmRepository.findByMember(findMember).orElseThrow(ConfirmNotFoundException::new);
 
         //when
         String token = findConfirm.getToken();
-        confirmTokenRequest = new ConfirmTokenRequest(findMember.getUsername(), findMember.getPassword(), token);
+        confirmTokenRequest = new ConfirmTokenRequest(createMemberRequest.getUsername(), createMemberRequest.getPassword(), token);
         memberService.joinConfirm(confirmTokenRequest);
 
         //then
@@ -98,14 +87,12 @@ class MemberServiceImplTest {
     @DisplayName("가입 승인 요청의 토큰과 저장된 토큰이 일치하지 않으면 가입 승인 요청이 실패해야 합니다.")
     void join_confirm_deny_test() {
         //given
-        Long memberId = memberService.join(createMemberRequest);
-        Member findMember = memberRepository.findById(memberId).orElseThrow(IllegalAccessError::new);
-        Confirm findConfirm = confirmRepository.findByMember(findMember);
+        memberService.join(createMemberRequest);
         TokenSender tokenSender = new TokenSenderImpl();
 
         //when //then
         String newToken = tokenSender.generateToken();
-        confirmTokenRequest = new ConfirmTokenRequest(findMember.getUsername(), findMember.getPassword(), newToken);
+        confirmTokenRequest = new ConfirmTokenRequest(createMemberRequest.getUsername(), createMemberRequest.getPassword(), newToken);
 
         assertThatThrownBy(() -> memberService.joinConfirm(confirmTokenRequest))
             .isInstanceOf(TokenMismatchException.class)
@@ -117,16 +104,16 @@ class MemberServiceImplTest {
     void join_confirm_no_user_test() {
         //given
         Long memberId = memberService.join(createMemberRequest);
-        Member findMember = memberRepository.findById(memberId).orElseThrow(IllegalAccessError::new);
-        Confirm findConfirm = confirmRepository.findByMember(findMember);
+        Member findMember = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
+        Confirm findConfirm = confirmRepository.findByMember(findMember).orElseThrow(ConfirmNotFoundException::new);
         String token = findConfirm.getToken();
 
         //when //then
         String wrongUsername = "aaa";
-        confirmTokenRequest = new ConfirmTokenRequest(wrongUsername, findMember.getPassword(), token);
+        confirmTokenRequest = new ConfirmTokenRequest(wrongUsername, createMemberRequest.getPassword(), token);
 
         assertThatThrownBy(() -> memberService.joinConfirm(confirmTokenRequest))
-            .isInstanceOf(IllegalAccessError.class)
+            .isInstanceOf(MemberNotFoundException.class)
             .describedAs("계정이 일치하지 않으면 가입 요청에 실패해야 합니다.");
 
     }
@@ -136,13 +123,13 @@ class MemberServiceImplTest {
     void join_confirm_mismatch_password_test() {
         //given
         Long memberId = memberService.join(createMemberRequest);
-        Member findMember = memberRepository.findById(memberId).orElseThrow(IllegalAccessError::new);
-        Confirm findConfirm = confirmRepository.findByMember(findMember);
+        Member findMember = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
+        Confirm findConfirm = confirmRepository.findByMember(findMember).orElseThrow(ConfirmNotFoundException::new);
         String token = findConfirm.getToken();
 
         //when //then
         String wrongPassword = "aaa";
-        confirmTokenRequest = new ConfirmTokenRequest(findMember.getUsername(), wrongPassword, token);
+        confirmTokenRequest = new ConfirmTokenRequest(createMemberRequest.getUsername(), wrongPassword, token);
 
         assertThatThrownBy(() -> memberService.joinConfirm(confirmTokenRequest))
             .isInstanceOf(PasswordMismatchException.class)
