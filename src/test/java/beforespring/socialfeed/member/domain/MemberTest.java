6301564abc7cf46a008da0e24fe3561bcd8cf627
Gape <45617104+jaekkang@users.noreply.github.com>
2023-10-28 13:41:31 +0900

    Feat: PasswordHasher 구현 ( Bcrypt ) (#48)
    * Init: Add dependency for BCrypt
    
    비밀번호 해시함수 라이브러리인 BCrypt 사용을 위한 Dependency 추가
    
    * Init: Create BcryptHasher class
    
    PasswordHasher를 implement한 BcryptHasher 를 생성
    
    * Style: Change method title
    
    matches보다 isMatch가 좀 나은거 같아서 수정했습니다.
    
    * Comment: Add comment for Bcrypt lib
    
    사용된 Bcrypt dependency에 대한 설명을 적어놨습니다.
    
    * Test: Add test for BcryptHasher
    
    다른 구현체로도 동일하게 동작하는 지에 대한 테스트를 한개 추가했습니다.
    
    * Refactor: remove hasher test in member entity
    
    멤버 테스트 상의 hasher 테스트를 지웠습니다.
    
    * Test: Add for BcryptHasher
    
    Bcrypt 구현체가 PasswordHasher를 정상적으로 구현했는지에 대한 테스트를 추가했습니다.

--- src/test/java/beforespring/socialfeed/member/domain/MemberTest.java
+++ src/test/java/beforespring/socialfeed/member/domain/MemberTest.java
@@ -1,29 +1,31 @@
 package beforespring.socialfeed.member.domain;
 
 
+import beforespring.socialfeed.member.infra.BcryptHasher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @DataJpaTest
 class MemberTest {
 
-    private PasswordHasher hasher;
+    private PasswordHasher defaultHasher;
 
     @BeforeEach
     public void setup() {
-        hasher = new PasswordHasher() {
+        defaultHasher = new PasswordHasher() {
             @Override
             public String hash(String toHash) {
                 return toHash;
             }
 
             @Override
-            public boolean matches(String raw, String hashed) {
+            public boolean isMatch(String raw, String hashed) {
                 return hash(raw).equals(hashed);
             }
         };
@@ -38,7 +40,7 @@ class MemberTest {
                             .username("User1")
                             .raw("1234")
                             .id(1L)
-                            .hasher(hasher)
+                            .hasher(defaultHasher)
                             .build();
 
         // when then
