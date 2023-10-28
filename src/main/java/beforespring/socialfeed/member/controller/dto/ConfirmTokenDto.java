917bc8a0ee4d29dec3be9ec4b78b9cafe0eaed17
Danbi <ddanhopark@gmail.com>
2023-10-28 15:58:29 +0900

    Init: Add ConfirmTokenDto
    ConfirmToken controller와 joinConfirm service에서 사용할 dto 추가
    Github issue #5

---
+++ src/main/java/beforespring/socialfeed/member/controller/dto/ConfirmTokenDto.java
@@ -0,0 +1,27 @@
+package beforespring.socialfeed.member.controller.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+
+public class ConfirmTokenDto {
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    static public class ConfirmTokenRequest {
+        @NotEmpty
+        private String username;
+        @NotEmpty
+        private String password;
+        @NotEmpty
+        private String token;
+
+        public ConfirmTokenRequest(String username, String password, String token) {
+            this.username = username;
+            this.password = password;
+            this.token = token;
+        }
+    }
+}
