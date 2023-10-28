5af7c3e0b84c634d79ba5b7f2e4a2e0239637392
Danbi <ddanhopark@gmail.com>
2023-10-28 21:43:59 +0900

    Feat: Add save member, save confirm
    db와 관련없는 로직은 트랜잭션에서 분리하여 구현함
    joinConfirm의 토큰 검증 코드와 패스워드 검증은 별도의 클래스에서 진행되도록 책임을 줄임.

--- src/main/java/beforespring/socialfeed/member/service/MemberServiceImpl.java
+++ src/main/java/beforespring/socialfeed/member/service/MemberServiceImpl.java
@@ -3,7 +3,10 @@ package beforespring.socialfeed.member.service;
 import beforespring.socialfeed.member.domain.*;
 import beforespring.socialfeed.member.exception.PasswordMismatchException;
 import beforespring.socialfeed.member.exception.TokenMismatchException;
+import beforespring.socialfeed.member.infra.BcryptHasher;
 import beforespring.socialfeed.member.infra.TokenSenderImpl;
+import beforespring.socialfeed.member.service.exception.ConfirmNotFoundException;
+import beforespring.socialfeed.member.service.exception.MemberNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,19 +15,18 @@ import static beforespring.socialfeed.member.controller.dto.ConfirmTokenDto.Conf
 import static beforespring.socialfeed.member.controller.dto.CreateMemberDto.CreateMemberRequest;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
     private final ConfirmRepository confirmRepository;
-    //private final PasswordHashe passwordhasher;
+//    private final PasswordHashe passwordhasher;
     //private final TokenSender tokenSender;
 
     @Override
     public Long join(CreateMemberRequest request) {
         //추후 주입 받는 것으로 변경
-        PasswordHasher passwordHasher = new PasswordHasherImpl();
+        PasswordHasher passwordHasher = new BcryptHasher();
         TokenSender tokenSender = new TokenSenderImpl();
 
         Member member = Member.builder()
@@ -32,27 +34,38 @@ public class MemberServiceImpl implements MemberService {
                             .raw(request.getPassword())
                             .hasher(passwordHasher)
                             .build();
-        Long memberId = memberRepository.save(member).getId();
+        Long memberId = saveMember(member);
+
         String token = tokenSender.generateToken();
-        tokenSender.sendEmail(request.getEmail(), token);
+
         Confirm confirm = Confirm.builder()
                               .member(member)
                               .token(token)
                               .build();
-        confirmRepository.save(confirm);
+        saveConfirm(confirm);
+        tokenSender.sendEmail(request.getEmail(), token);
         return memberId;
     }
 
+    @Transactional
+    public Long saveMember(Member member) {
+        return memberRepository.save(member).getId();
+    }
+
+    @Transactional
+    public Long saveConfirm(Confirm confirm) {
+        return confirmRepository.save(confirm).getId();
+    }
+
     @Override
+    @Transactional
     public void joinConfirm(ConfirmTokenRequest request) {
-        Member member = memberRepository.findByUsername(request.getUsername()).orElseThrow(IllegalAccessError::new);
-        Confirm confirm = confirmRepository.findByMember(member);
+        Member member = memberRepository.findByUsername(request.getUsername()).orElseThrow(MemberNotFoundException::new);
+        Confirm confirm = confirmRepository.findByMember(member).orElseThrow(ConfirmNotFoundException::new);
         //추후 주입 받는 것으로 변경
-        PasswordHasher passwordHasher = new PasswordHasherImpl();
-        if (!passwordHasher.matches(request.getPassword(), member.getPassword()))
-            throw new PasswordMismatchException();
-        if (!confirm.getToken().equals(request.getToken()))
-            throw new TokenMismatchException();
+        PasswordHasher passwordHasher = new BcryptHasher();
+        member.verifyPassword(request.getPassword(), passwordHasher);
+        confirm.verifyToken(request.getToken());
         member.joinConfirm();
     }
 }
