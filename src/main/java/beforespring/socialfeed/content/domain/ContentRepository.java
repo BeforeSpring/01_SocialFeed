8c212f5b6cd71f2ff20114595a2eb4d5f0244e78
yhk313 <kyeong4099@naver.com>
2023-10-29 10:13:20 +0900

    Chore: 좋아요 기능 비관적 락 적용

--- src/main/java/beforespring/socialfeed/content/domain/ContentRepository.java
+++ src/main/java/beforespring/socialfeed/content/domain/ContentRepository.java
@@ -1,9 +1,17 @@
 package beforespring.socialfeed.content.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.LockModeType;
+import java.util.Optional;
 
 @Repository
 public interface ContentRepository extends JpaRepository<Content, Long> {
-
+    /**
+     * 비관적 락을 사용하여 동시 업데이트 문제 방지
+     */
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Content> findById(Long id);
 }
