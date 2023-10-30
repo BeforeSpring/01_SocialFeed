17d38bd7713ba90d674c43e58e9f51dcd793155d
Seonghyeon Kim <kshshkim@users.noreply.github.com>
2023-10-30 15:34:31 +0900

    Feat: ContentController 쿼리 API 외 (#80)
    - ApplicationConversionService 추가

--- src/main/java/beforespring/socialfeed/web/api/v1/content/ContentControllerAdvice.java
+++ src/main/java/beforespring/socialfeed/web/api/v1/content/ContentControllerAdvice.java
@@ -2,6 +2,7 @@ package beforespring.socialfeed.web.api.v1.content;
 
 import beforespring.socialfeed.web.api.v1.response.ErrorResponse;
 import beforespring.socialfeed.content.service.exception.ContentNotFoundException;
+import beforespring.socialfeed.web.memberinforesolver.MemberInfoResolverException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,6 +16,12 @@ public class ContentControllerAdvice {
     @ExceptionHandler(ContentNotFoundException.class)
     public ErrorResponse<String> handleContentNotFoundException(ContentNotFoundException e, HttpServletRequest request) {
         return new ErrorResponse<>(HttpStatus.NOT_FOUND, e.getMessage(), request.getRequestURI());
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(MemberInfoResolverException.class)
+    public ErrorResponse<String> memberInfoResolverException(MemberInfoResolverException e, HttpServletRequest request) {
+        return new ErrorResponse<>(HttpStatus.UNAUTHORIZED, "unauthorized", request.getRequestURI());
     }
 }
 
