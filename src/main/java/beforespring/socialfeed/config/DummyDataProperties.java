db9649bec712a6566f75da252e23de8e83cc225d
Seonghyeon Kim <kshshkim@users.noreply.github.com>
2023-10-30 14:41:59 +0900

    Fix: 더미 데이터 잘못 생성되는 문제 (#77)
    * Feat: 컨텐츠 통계 기능
    
    - 쿼리 파라미터, 응답 스펙 지정
    - 기간별 태그, 좋아요, 공유, 조회 숫자 통계
    
    * Fix: spel 문법 오류로 DefaultValue가 잘못 적용되는 문제

--- src/main/java/beforespring/socialfeed/config/DummyDataProperties.java
+++ src/main/java/beforespring/socialfeed/config/DummyDataProperties.java
@@ -2,11 +2,13 @@ package beforespring.socialfeed.config;
 
 import java.util.List;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 
 
+@Slf4j
 @Getter
 @ConstructorBinding
 @ConfigurationProperties(prefix = "social-feed.dummy")
@@ -19,7 +21,7 @@ public class DummyDataProperties {
     public DummyDataProperties(
         @DefaultValue("1000") int totalContent,
         @DefaultValue("20") int maxHashtagCountPerContent,
-        @DefaultValue("wanted,pre_onboarding,backend,before_spring") List<String> readableHashtags
+        @DefaultValue({"wanted", "pre_onboarding", "backend", "before_spring"}) List<String> readableHashtags
     ) {
         this.totalContent = totalContent;
         this.maxHashtagCountPerContent = maxHashtagCountPerContent;
